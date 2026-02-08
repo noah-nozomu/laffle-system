8e3dec34982f5ad21b4a293d54ae4fbaff8d9e1d
noah_nozomu <nozomu.tsukakoshi@gmail.com>
2026-02-08 11:28:44 +0900

    Fix urls for images

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
+    # メディアファイル（写真）を強制的に表示する設定
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
-
-# 画像を表示するための設定（デバッグモード時のみ）
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
